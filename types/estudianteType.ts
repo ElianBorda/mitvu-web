d9295136ad770c89b00a7669cdb07cf088ec58fb
ElianBorda <eliancamiloalejandro@gmail.com>
2026-05-07 18:40:05 -0300

    update: se elimina el campo diaHabil

---
+++ types/estudianteType.ts
@@ -0,0 +1,16 @@
+export type EstudianteBody = {
+    nombre: string,
+    apellido: string,
+    mail: string,
+    carrera: string,
+    comision_id: string
+}
+
+export type Estudiante = {
+    id: string,
+    nombre: string,
+    apellido: string,
+    mail: string,
+    carrera: string,
+    comision_id: string
+}
---
+++ types/eventoType.ts
@@ -0,0 +1,12 @@
+export type EventoBody = {
+    titulo: string,
+    descripcion: string,
+    fecha: string,
+}
+
+export type Evento = {
+    id: string,
+    titulo: string,
+    descripcion: string,
+    fecha: string,
+}
